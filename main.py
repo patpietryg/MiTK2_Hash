cf4c810845b3927efa7b7b7a4e9d0961867eaf89
patpietryg <patpietryg@gmail.com>
2024-03-17 21:33:12 +0100

    final hash_file function

--- main.py
+++ main.py
@@ -11,15 +11,6 @@ def hash_data(data):
     Returns:
         list: A list containing pairs of hash values and their corresponding execution times.
               Each pair is represented as [hashed_data (str), execution_time (float)].
-
-    Example:
-        >>> hash_data("Hello, world!")
-        [['2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824', 1.698142359],
-         ['6dcd4ce23d88e2ee9568ba546c007c63d9131c1b250b8f5f2e4dc4c5c30f89dc', 1.349122872],
-         ['f7ff9e8b7bb2e09b70935a5d785e0cc5d9d0abf0c54a7a0b2bdc59026a00781', 1.315445931],
-         ...
-         ['sha3_384 is not an available hash function in hashlib.', 0],
-         ...]
     """
     hashes = []
     for algorithm in hashlib.algorithms_available:
@@ -37,10 +28,37 @@ def hash_data(data):
             hashes.append([f"{algorithm} is not an available hash function in hashlib.", 0])
     return hashes
 
+
+def hash_file(file_path):
+    """
+    Calculate the hash of the specified binary file using SHA-256 algorithm.
+
+    Args:
+        file_path (str): The path to the binary file.
+
+    Returns:
+        str: The SHA-256 hash value of the file.
+
+    Raises:
+        FileNotFoundError: If the specified file does not exist.
+    """
+    try:
+        with open(file_path, "rb") as file:
+            file_data = file.read()
+            hash_value = hashlib.sha256(file_data).hexdigest()
+            return hash_value
+    except FileNotFoundError:
+        raise FileNotFoundError(f"The specified file '{file_path}' does not exist.")
+
+
 def main():
     data = input("Podaj dane do zahaszowania: ")
     x = hash_data(data)
     print(x)
+
+    file_path = r"C:\Users\patpi\ubuntu-23.10.1-desktop-amd64.iso"
+    hashed_value = hash_file(file_path)
+    print("Hash value:", hashed_value)
 
 if __name__ == "__main__":
     main()
